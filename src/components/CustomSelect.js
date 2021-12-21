86dc1d86b4c975d4a78b4d2e122f29bb0970a417
LekshmiSubramonian <72738080+LekshmiSubramonian@users.noreply.github.com>
2021-12-21 13:29:49 +0530

    Add files via upload

---
+++ src/components/CustomSelect.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Select from 'react-select'
+
+function CustomSelect() {
+    return (
+        <div>
+            <Select/>
+        </div>
+    )
+}
+
+export default CustomSelect
